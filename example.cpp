511af42efd7f8e84c50af2103d38ac0834b7cb90
sagi <sagi21805@gmail.com>
2024-06-22 16:43:57 +0300

    changed the example to measure time and utilize the matrix class

--- example.cpp
+++ example.cpp
@@ -1,18 +1,30 @@
-#include "matmul_npu.hpp"
+#include "matrix_types/matrix.hpp"
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 int main() {
 
-    int rows = 32;
-    int cols = 32;
+    int rows = 4096;
+    int cols = 4096;
 
-    std::vector<float16> a(rows*cols, 0.2);
+    std::vector<int8_t> a(rows*cols, 2);
 
-    std::vector<float16> b(rows*cols, 0.3);
+    std::vector<int8_t> b(rows*cols, 3);
 
-    _matmul_ctx<float32>* ctx = matmul_npu<float32>(rows, cols, rows, a.data(), b.data());
+    Matrix<int8_t> A(rows, cols, a.data());
+    Matrix<int8_t> B(rows, cols, b.data());
 
-    std::cout << "The first item of the matrix: \n";
-    std::cout << ctx->result[0] << "\n";
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    Matrix<int32_t> C = A.matmul<int32_t>(B);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    std::cout << "Time took: " << elapsed.count() / 1.0E9f << "s\n";
+
+    std::cout << "The first item of the matrix: ";
+    std::cout << C.data[0] << "\n";
 }
